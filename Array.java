1eee45f9968501019ef5e9440276a820e02d04bc
Nikhil-Coder2k25 <bhatinikhilsingh@gmai.com>
2025-06-19 13:44:41 +0530

    Updated

---
+++ Array.java
@@ -0,0 +1,28 @@
+public class Array{
+public static void main(String args[]) {
+int[] myArr = new int[10];
+myArr[0]= 98;
+myArr[9]= 76;
+myArr[8]= 87;
+myArr[7]= 94;
+myArr[6]= 45;
+myArr[2]= 91;
+myArr[3]= 97;
+myArr[5]= 12;
+myArr[4]= 15;
+myArr[1]= 55;
+/* or 
+int[] myArr={98,76,87,94,45,91,97,12,15,55}; 
+*/
+System.out.println(myArr[0]);
+System.out.println(myArr[1]);
+System.out.println(myArr[2]);
+System.out.println(myArr[3]);
+System.out.println(myArr[4]);
+System.out.println(myArr[5]);
+System.out.println(myArr[6]);
+System.out.println(myArr[7]);
+System.out.println(myArr[8]);
+System.out.println(myArr[9]);
+}
+}
